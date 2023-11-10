0fbaa8e534e16659ba3309d5eca55a09033b7c05
iribel97 <irinaochoa32@gmail.com>
2023-11-10 09:04:44 -0500

    Crear Proyecto

---
+++ src/main/java/com/iri/CarteleraCine2/CarteleraCine2Application.java
@@ -0,0 +1,13 @@
+package com.iri.CarteleraCine2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteleraCine2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteleraCine2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iri/CarteleraCine2/CarteleraCine2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iri.CarteleraCine2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteleraCine2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
